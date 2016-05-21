80453eb9740793e84c5f0a5b2d6432e291b9e04a
ranyu <ranyu2@126.com>
2016-05-21 12:51:56 +0800

    split train and validation

--- code/split_train_validation.sh
+++ code/split_train_validation.sh
@@ -2,6 +2,6 @@
 for q in `ls ../play_number/`
 do
     echo $q
-    head -n 154 ../play_number/$q > ../local_train/$q 
-    tail -n 30 ../play_number/$q > ../local_vali/$q
+    head -n 154 ../play_number/$q > ../local_train/train_$q 
+    tail -n 30 ../play_number/$q > ../local_vali/vali_$q
 done
